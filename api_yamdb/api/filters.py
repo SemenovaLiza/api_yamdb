1918220fdb1cda2e4829954d39d8dedebe40bf02
Владимир Захаров <zakharov_v_s@hotmail.com>
2023-03-05 23:16:28 +0700

    100% Tests

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,14 +1,19 @@
+"""A module for filtering functions."""
 import django_filters
 
 from reviews.models import Title
 
 
 class TitleFilter(django_filters.FilterSet):
+    """A class that filters a list of Title strings."""
+
     name = django_filters.CharFilter(field_name='name')
-    genre = django_filters.CharFilter(field_name='genre')
-    category = django_filters.CharFilter(field_name='category')
+    genre = django_filters.CharFilter(field_name='genre__slug')
+    category = django_filters.CharFilter(field_name='category__slug')
     year = django_filters.NumberFilter(field_name='year')
 
     class Meta:
+        """A metaclass for the TitleFilter class."""
+
         model = Title
         fields = '__all__'
--- api_yamdb/reviews/management/commands/import.py
+++ api_yamdb/reviews/management/commands/import.py
@@ -1,7 +1,10 @@
+"""Import CSV."""
+
 import csv
 from django.conf import settings
 from django.core.management import BaseCommand
-from reviews.models import (Category, Genre, Title, CustomUser, Review, Comment, GenreTitle)
+from reviews.models import (Category, Genre, Title, CustomUser,
+                            Review, Comment, GenreTitle)
 
 SOURCES = {
     CustomUser: 'users.csv',
@@ -15,15 +18,16 @@ SOURCES = {
 
 
 class Command(BaseCommand):
+    """A subclass of Django's BaseCommand."""
+
     help = 'CSV Import'
 
     def handle(self, *args, **options):
+        """Import CSVs when the command is entered."""
         for model, source in SOURCES.items():
             with open(
                 f'{settings.BASE_DIR}/static/data/{source}',
-                encoding='utf-8') as f:
-                '''Объект чтения в качестве словаря'''
+                    encoding='utf-8') as f:
+                '''Reading object as a dictionary.'''
                 reader = csv.DictReader(f)
                 model.objects.bulk_create(model(**data) for data in reader)
-
-        self.stdout.write(self.style.SUCCESS('Загрузка успешна'))
